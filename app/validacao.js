42e6ae1e4adbe2cb4a4fc2f4fe640a2243998f69
RA4Z <rraz639@gmail.com>
2023-05-13 11:47:52 -0300

    Criação do botão para recomeçar o jogo

--- app/validacao.js
+++ app/validacao.js
@@ -16,6 +16,8 @@ function verificaSeValorValido(chute) {
         document.body.innerHTML = `
             <h2>Você acertou!</h2>
             <h3>O número secreto era ${numeroSecreto}!</h3>
+
+            <button id="jogar-novamente" class="btn-jogar">Jogar Novamente</button>
         `
     } else if (numero > numeroSecreto) {
         elementoChute.innerHTML += `
@@ -35,3 +37,8 @@ function chuteForInvalido(numero) {
 function numeroForMaiorOuMenorQueOValorPermitido(numero) {
     return numero > maiorValor || numero < menorValor
 }
+document.body.addEventListener('click', e => {
+    if (e.target.id == 'jogar-novamente') {
+        window.location.reload()
+    }
+})
